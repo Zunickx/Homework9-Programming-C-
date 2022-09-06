766e7a91903798d0ce5ea58025602a0f011583fa
Zunickx <Zunickx@gmail.com>
2022-09-06 20:53:30 +0300

    Task001 Вывести все натуральные числа в промежутке от M до N

---
+++ Task001/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N
+
+Console.Write("Введите число M: ");
+string numberN = Console.ReadLine()!;
+Console.Write("Введите число N: ");
+string numberM = Console.ReadLine()!;
+int m = int.Parse(numberN);
+int n = int.Parse(numberM);
+
+void PrintNaturalNumbers(int m, int n)
+{
+    if (m == n) Console.Write($"{n} "); 
+    else 
+    { 
+        Console.Write($"{m} "); 
+        PrintNaturalNumbers(m + 1, n); 
+    } 
+}
+
+PrintNaturalNumbers(m, n);
